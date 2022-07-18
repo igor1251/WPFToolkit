be0e539de08935fadcc5c3e150e70de814ebea30
igor1251 <84015785+igor1251@users.noreply.github.com>
2022-07-18 17:57:20 +0300

    Реализован TImePicker

---
+++ WPFToolkit/WPFToolkit.NetCore/Controls/TimePickerControl.xaml.cs
@@ -0,0 +1,58 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFToolkit.NetCore.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для TimePickerControl.xaml
+    /// </summary>
+    [INotifyPropertyChanged]
+    public partial class TimePickerControl : UserControl
+    {
+        public static readonly DependencyProperty HoursProperty =
+            DependencyProperty.Register("Hours", typeof(UInt16), typeof(TimePickerControl));
+
+        public static readonly DependencyProperty MinutesProperty =
+            DependencyProperty.Register("Minutes", typeof(UInt16), typeof(TimePickerControl));
+
+        public static readonly DependencyProperty SecondsProperty =
+            DependencyProperty.Register("Seconds", typeof(UInt16), typeof(TimePickerControl));
+
+        public static readonly DependencyProperty TimeProperty =
+            DependencyProperty.Register("Time", typeof(DateTime), typeof(TimePickerControl));
+
+        const string TIME_VALIDATION_REGEX = @"^([0-2][0-4]:[0-5][0-9]:[0-5][0-9])$";
+
+        string timeString = "00:00:00";
+        public string TimeString
+        {
+            get => timeString;
+            set
+            {
+                if (Regex.IsMatch(value, TIME_VALIDATION_REGEX))
+                {
+                    SetProperty(ref timeString, value);
+                }
+            }
+        }
+        
+        public TimePickerControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
